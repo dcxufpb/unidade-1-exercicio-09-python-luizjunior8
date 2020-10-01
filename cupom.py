f42289cc3a5c5865fc9ddb93f707855b66648b76
luizjunior8 <luiz.oliveira@dcx.ufpb.br>
2020-10-01 01:32:59 -0300

    Mission Accomplished

--- cupom.py
+++ cupom.py
@@ -19,5 +19,63 @@ class Loja:
     self.inscricao_estadual = inscricao_estadual
 
   def dados_loja(self):
-    # Implemente aqui
-    return "" 
+
+    if not self.nome_loja:
+        raise Exception("O campo nome da loja é obrigatório")
+
+    if not self.logradouro:
+        raise Exception("O campo logradouro do endereço é obrigatório") 
+
+    _numero = "s/n" if not self.numero else str(self.numero)
+
+    if not self.municipio:
+        raise Exception("O campo município do endereço é obrigatório")  
+
+    if not self.estado:
+        raise Exception("O campo estado do endereço é obrigatório") 
+
+    if not self.cnpj:
+        raise Exception("O campo CNPJ da loja é obrigatório") 
+
+    if not self.inscricao_estadual:
+        raise Exception("O campo inscrição estadual da loja é obrigatório")  
+
+
+    _complemento = ""
+
+    if self.complemento:
+       _complemento = " " + self.complemento
+
+
+    _bairro = ""
+
+    if self.bairro:
+        _bairro = self.bairro + " - "
+
+    _cep = ""
+    _telefone = ""
+
+    if self.cep:
+        _cep = "CEP:" + self.cep
+
+        if self.telefone:
+            _telefone = " Tel " + self.telefone
+
+    else:
+        _telefone = "Tel " + self.telefone
+
+
+    _observacao = ""
+
+    if self.observacao:
+        _observacao = self.observacao
+
+    typewriter = self.nome_loja + "\n"
+    typewriter += self.logradouro + ", " + _numero + _complemento + "\n"
+    typewriter += _bairro + self.municipio + " - " + self.estado + "\n"
+    typewriter += _cep + _telefone + "\n"
+    typewriter += _observacao + "\n"
+    typewriter += "CNPJ: " + self.cnpj + "\n"
+    typewriter += "IE: " + self.inscricao_estadual
+
+    return typewriter 
